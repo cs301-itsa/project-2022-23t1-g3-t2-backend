a17c202d89e689826ec09c1e4112d95ef8fea66a
EC2 Default User <ec2-user@ip-172-31-11-144.ec2.internal>
2022-10-13 09:22:30 +0000

    test3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from yours, CS301 team!'),
+        body: JSON.stringify('Hello from yours, CS301 teams!'),
     };
     return response;
 };
